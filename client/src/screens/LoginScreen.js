683d705e3e21b41b16c738cf6de969d9d5404bf6
faseehahmad00 <faseehahmad00@gmail.com>
2021-06-15 18:58:00 +0500

    redesigned signup/login using useforms

--- client/src/screens/LoginScreen.js
+++ client/src/screens/LoginScreen.js
@@ -1,4 +1,5 @@
 import { Container } from '@material-ui/core';
+import { useForm, Controller } from "react-hook-form";
 import Topbarlogin from '../components/topbarlogin';
 import TextField from '@material-ui/core/TextField';
 import Link from '@material-ui/core/Link';
@@ -9,51 +10,36 @@ import { useHistory } from "react-router-dom";
 
 const LoginScreen = () => {
     let history = useHistory();
-
     let [islogin, setislogin] = useState(true);
-    let [name, setname] = useState('');
-    let [email, setmail] = useState('');
-    let [password, setpassword] = useState('');
-    let [nameerr, setnameerr] = useState(false);
-    let [emailerr, setmailerr] = useState(false);
-    let [passworderr, setpassworderr] = useState(false);
     let [disabled, setdisabled] = useState(false);
     let [formsaving, setformsaving] = useState(false)
 
+    const { register, control, handleSubmit, formState: { errors } } = useForm();
+
+
     function togglelogin() {
-        setnameerr(false)
-        setmailerr(false)
-        setpassworderr(false)
-        setmail('')
-        setpassword('')
         setislogin(true)
     }
 
 
     function togglesignup() {
-        setnameerr(false)
-        setmailerr(false)
-        setpassworderr(false)
-        setname('')
-        setmail('')
-        setpassword('')
         setislogin(false)
     }
 
-    function submitlogin() {
-        if (validatemail() && validatepassword())
+    function submitlogin({email,password}) {
+        console.log(email,password)
         setdisabled(true);
         setformsaving(true);
-        axios.post('/api/users/login', {
-            email: email,
-            password: password
+        axios.post('/api/users/login',{
+             email :email,
+            password:password
         })
             .then(function (response) {
                 setdisabled(false);
                 setformsaving(false);
                 history.push({
                     pathname: '/app',
-                    state: { token : response.data}
+                    state: { token: response.data }
                 });
             })
             .catch(function (error) {
@@ -63,15 +49,11 @@ const LoginScreen = () => {
             })
     }
 
-    function submitsignup() {
-        if (validatename() && validatemail() && validatepassword()) {
+    function submitsignup(data) {
+            console.log(data)
             setdisabled(true);
             setformsaving(true);
-            axios.post('/api/users/signup', {
-                name: name,
-                email: email,
-                password: password
-            })
+            axios.post('/api/users/signup',data)
                 .then(function (response) {
                     console.log(response);
                     togglelogin()
@@ -83,83 +65,125 @@ const LoginScreen = () => {
                     setdisabled(false);
                     setformsaving(false);
                 });
-
-        }
     }
 
-    function validatename() {
-        setnameerr(false)
-        if (name.length < 3) setnameerr(true)
-        else
-            return true
-    }
 
-    function validatemail() {
-        setmailerr(false)
-        if (email.includes('@') && email.length > 5 && email.includes('.'))
-            return true
-        else
-            setmailerr(true)
-    }
-
-    function validatepassword() {
-        setpassworderr(false)
-        if (password.length < 3 || password.length >= 20)
-            setpassworderr(true)
-        else
-            return true
-    }
 
     return (
         <div>
             <Topbarlogin togglesignup={togglesignup} togglelogin={togglelogin} disabled={disabled} showprogress={formsaving} />
+
             {islogin && <div>
                 <Container maxWidth={'xs'} style={{ padding: '5rem' }}>
                     <h2>WELCOME TO LOGIN</h2>
-                    <TextField id="email" error={emailerr} autoComplete='false' type="email" label="Email" fullWidth style={{ margin: '2rem 0 1rem 0' }}
-                        onChange={(e) => {
-                            setmail(e.target.value)
-                            validatemail()
-                        }} />
-                    <TextField id="password" error={passworderr} autoComplete='off' type="password" label="Password" fullWidth style={{ margin: '2rem 0 0.5rem 0' }}
-                        onChange={(e) => {
-                            setpassword(e.target.value)
-                            validatepassword()
-                        }} />
-                    <Link style={{ textAlign: 'left' }} onClick={togglesignup}>
-                        <p>new user ? click to signup</p>
-                    </Link>
-                    <Button disabled={disabled} variant="contained" style={{ marginTop: '3rem' }} color="secondary" fullWidth onClick={submitlogin}>
-                        Login
-                    </Button>
+                    <form onSubmit={handleSubmit(submitlogin)}>
+
+                        <Controller
+                            name="email"
+                            control={control}
+                            defaultValue=''
+                            render={({ field }) => <TextField
+                                type='email'
+                                {...register("email", { minLength: 7 })}
+                                error={errors.email ? true : false}
+                                fullWidth label='email' {...field}
+                                style={{ marginTop: "2rem" }}
+                            />
+                            }
+                        />
+
+                        <Controller
+                            name="password"
+                            control={control}
+                            defaultValue=''
+                            render={({ field }) => <TextField
+                                type='password'
+                                {...register("password", { minLength: 5 })}
+                                error={errors.password ? true : false}
+                                fullWidth label='password' {...field}
+                                style={{ marginTop: "2rem" }}
+                            />
+                            }
+                        />
+                        <Link style={{ textAlign: 'left' }} onClick={togglesignup}>
+                            <p>new user ? click to signup</p>
+                        </Link>
+
+                        <Button
+                            disabled={disabled}
+                            style={{ marginTop: '2rem' }}
+                            color='secondary'
+                            type='submit'
+                            variant='contained'
+                            fullWidth
+                        >
+                            LOGIN
+                        </Button>
+                    </form>
                 </Container>
             </div>}
 
 
             {!islogin && <div>
-                <Container maxWidth={'xs'} style={{ padding: '4rem' }}>
+                <Container maxWidth={'xs'} style={{ padding: '5rem' }}>
                     <h2>WELCOME TO SIGNUP</h2>
-                    <TextField error={nameerr} autoComplete='off' id="name" label="name" fullWidth style={{ margin: '2rem 0 1rem 0' }}
-                        onChange={(e) => {
-                            setname(e.target.value)
-                            validatename()
-                        }} />
-                    <TextField error={emailerr} autoComplete='off' id="email" label="Email" fullWidth style={{ margin: '2rem 0 1rem 0' }}
-                        onChange={(e) => {
-                            setmail(e.target.value)
-                            validatemail()
-                        }} />
-                    <TextField error={passworderr} autoComplete='off' id="password" type="password" label="Password" fullWidth style={{ margin: '2rem 0 0.5rem 0' }}
-                        onChange={(e) => {
-                            setpassword(e.target.value)
-                            validatepassword()
-                        }} />
-                    <Link style={{ textAlign: 'left' }} onClick={togglelogin}>
-                        <p>already a user ? click to login</p>
-                    </Link>
-                    <Button variant="contained" color="secondary" style={{ marginTop: '3rem' }} fullWidth onClick={submitsignup}>
-                        Signup
-                    </Button>
+                    <form onSubmit={handleSubmit(submitsignup)}>
+                        <Controller
+                            name="name"
+                            control={control}
+                            defaultValue=''
+                            render={({ field }) => <TextField
+                                required
+                                type='text'
+                                {...register("name", { minLength: 3 })}
+                                error={errors.name ? true : false}
+                                fullWidth label='name' {...field}
+                                style={{ marginTop: "2rem" }}
+                            />
+                            }
+                        />
+                        <Controller
+                            name="email"
+                            control={control}
+                            defaultValue=''
+                            render={({ field }) => <TextField
+                                type='email'
+                                {...register("email", { minLength: 7 })}
+                                error={errors.email ? true : false}
+                                fullWidth label='email' {...field}
+                                style={{ marginTop: "2rem" }}
+                            />
+                            }
+                        />
+
+                        <Controller
+                            name="password"
+                            control={control}
+                            defaultValue=''
+                            render={({ field }) => <TextField
+                                type='password'
+                                {...register("password", { minLength: 5 })}
+                                error={errors.password ? true : false}
+                                fullWidth label='password' {...field}
+                                style={{ marginTop: "2rem" }}
+                            />
+                            }
+                        />
+                        <Link style={{ textAlign: 'left' }} onClick={togglelogin}>
+                            <p>already a user ? click to login</p>
+                        </Link>
+
+                        <Button
+                            disabled={disabled}
+                            style={{ marginTop: '2rem' }}
+                            color='secondary'
+                            type='submit'
+                            variant='contained'
+                            fullWidth
+                        >
+                            Signup
+                        </Button>
+                    </form>
                 </Container>
             </div>}
         </div>
--- routes/users.js
+++ routes/users.js
@@ -11,18 +11,18 @@ const adminAuth = require('../middlewares/adminAuth');
 /* GET users listing. */
 router.get('/' ,async function(req, res, next) {
   let users = await User.find();
-  res.send(users);
+  return res.send(users);
 });
 
 router.get('/:id', userAuth ,async function(req, res, next) {
   let user = await User.findById(req.params.id);
-  res.send(user);
+  return res.send(user);
 });
 
 router.delete('/:id',userAuth,adminAuth,async function(req, res, next) {
   let user = await User.findById(req.params.id);
   await user.delete();
-  res.send({});
+  return res.send({});
 });
 
 router.put('/:id',signupValidation,userAuth,adminAuth,async function(req, res, next) {
@@ -32,7 +32,7 @@ router.put('/:id',signupValidation,userAuth,adminAuth,async function(req, res, n
   user.password = await Encrypt(req.body.password);
   user.role = req.body.role;
   a = await user.save();
-  res.send(a);
+  return res.send(a);
 });
 
 router.post('/signup',signupValidation,async function(req, res, next) {
@@ -45,22 +45,22 @@ router.post('/signup',signupValidation,async function(req, res, next) {
     user.role = req.body.role;
     await user.save();
     let {name,email}  = user ;
-    res.status(200).send({name,email});
+    return res.status(200).send({name,email});
   }
-  res.status(400).send("user already exist");
+  return res.status(400).send("user already exist");
 
 });
 
 router.post('/login',loginValidation,async function(req, res, next) {
   let user = await User.findOne({"email":req.body.email});
   if(!user)
-    res.status(400).send("invalid user or password"); 
+    return res.status(400).send("invalid user or password"); 
   else
     console.log('user exists')
     if(await bcrypt.compare(req.body.password, user.password))
     {
     let token = jwt.sign({_id : user._id},config.get("privateKey"));
-    res.send(token)
+    return res.send(token)
     }
     else
       return res.status(400).send("invalid password ");
